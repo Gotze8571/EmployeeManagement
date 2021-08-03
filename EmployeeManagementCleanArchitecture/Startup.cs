5e27d4318e41c488041f601214e1beab8b6fb908
Tella Adesina <telladesina100@gmail.com>
2021-08-03 10:00:52 -0500

    Commit03082021-GHYu

--- EmployeeManagementCleanArchitecture/Startup.cs
+++ EmployeeManagementCleanArchitecture/Startup.cs
@@ -53,12 +53,12 @@ namespace EmployeeManagementCleanArchitecture
                 app.UseDeveloperExceptionPage();
                 //app.UseHttpsRedirection();
             }
-            //else
-            //{
-            //    app.UseExceptionHandler("/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee.API v1"));
