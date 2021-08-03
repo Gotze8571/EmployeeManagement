852b1120b9b61e06cdbec0a513cc54da1875b8ff
Tella Adesina <telladesina100@gmail.com>
2021-08-03 09:59:12 -0500

    Commit03082021-GHYT

--- EmployeeManagementCleanArchitecture/Startup.cs
+++ EmployeeManagementCleanArchitecture/Startup.cs
@@ -52,7 +52,6 @@ namespace EmployeeManagementCleanArchitecture
             {
                 app.UseDeveloperExceptionPage();
                 //app.UseHttpsRedirection();
-
             }
             //else
             //{
