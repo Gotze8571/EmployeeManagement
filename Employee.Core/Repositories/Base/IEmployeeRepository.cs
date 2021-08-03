baa3fb2b442d81a9772ce25bae9791a7d837c1e9
Tella Adesina <telladesina100@gmail.com>
2021-08-03 05:26:33 -0500

    Commit03082021-HN

---
+++ Employee.Core/Repositories/Base/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Core.Repositories.Base
+{
+    public interface IEmployeeRepository : IRepository<Employee.Core.Entities.Employee>
+    {
+        // custom operations here
+        Task<IEnumerable<Employee.Core.Entities.Employee>> GetEmployeesByLastName(string lastname);
+    }
+}
